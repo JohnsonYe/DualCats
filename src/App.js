2dd3a8bb94ff87d185ddd03d0a7131ae30960923
Brian <briankhi829@gmail.com>
2020-10-07 16:03:02 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -118,13 +118,17 @@ class App extends Component {
           {/* <div className="app-header"> */}
             <nav className='navbar'>
               <div><Link to={'/'} className="nav-link nav-logo"><h1>Dualcats</h1></Link></div>
-              <ul>
-                <li className='navbar-list home'><Link to={'/'} className="nav-link">Home</Link></li>
-                {/* <li className='navbar-list contact'><Link to={'/contact'} className="nav-link">Contact</Link></li> */}
-                {/* <li className='navbar-list aboutme'><Link to={'/aboutme'} className="nav-link">About Me</Link></li> */}
-                <li className='navbar-list catsGallery'><Link to={'/catsGallery'} className="nav-link">Gallery</Link></li>
-                {/* <li className='navbar-list myPets'><Link to={'/catsGallery'} className="nav-link">My Pets</Link></li> */}
-              </ul>
+              <div className='icon'>
+                <ul>
+                  <li className='navbar-list home active'><Link to={'/'} className="nav-link">Home</Link></li>
+                  {/* <li className='navbar-list contact'><Link to={'/contact'} className="nav-link">Contact</Link></li> */}
+                  {/* <li className='navbar-list aboutme'><Link to={'/aboutme'} className="nav-link">About Me</Link></li> */}
+                  <li className='navbar-list catsGallery'><Link to={'/catsGallery'} className="nav-link">Gallery</Link></li>
+                  {/* <li className='navbar-list myPets'><Link to={'/catsGallery'} className="nav-link">My Pets</Link></li> */}
+                  <hr />
+                </ul>
+              </div>
+             
               <div className="sign-in-section" >
                 {signInSection}
               </div>
